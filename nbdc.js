e79a2f5d4a0138901075b3ae2a7c037ecb73d689
butera-simone <buterasimone@proton.me>
2023-04-12 01:55:55 +0200

    switched nbdc to spawnsync

--- nbdc.js
+++ nbdc.js
@@ -1,17 +1,23 @@
 #!/usr/bin/env node
 
-const { exec } = require('child_process')
+const { spawnSync } = require('child_process')
 
-const command = `sudo modprobe nbd && sudo nbd-client -d ${process.argv[3]} && sudo nbd-client -N export1 -unix ${process.argv[2]} ${process.argv[3]} -b 1024`
+const modprobe = spawnSync('sudo', ['modprobe', 'nbd'])
+if (modprobe.error) {
+  console.error(`Error: ${modprobe.error}`)
+  process.exit(1)
+}
 
-exec(command, (error, stdout, stderr) => {
-  if (error) {
-    console.log(error.message)
-    return
-  }
-  if (stderr) {
-    console.log(stderr)
-    return
-  }
-  console.log(stdout)
-})
+const disconnect = spawnSync('sudo', ['nbd-client', '-d', process.argv[3]])
+if (disconnect.error) {
+  console.error(`Error: ${disconnect.error}`)
+  process.exit(1)
+}
+
+const connect = spawnSync('sudo', ['nbd-client', '-N', 'export1', '-unix', process.argv[2], process.argv[3], '-b', '1024'
+])
+if (connect.error) {
+  console.error(`Error: ${connect.error}`)
+  process.exit(1)
+}
+console.log(connect.stdout.toString())
