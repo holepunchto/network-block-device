46000a9490be4f84293f4f337d87badb5899d86d
butera-simone <buterasimone@proton.me>
2023-04-12 02:11:58 +0200

    fixed error handling

--- nbdc.js
+++ nbdc.js
@@ -2,22 +2,21 @@
 
 const { spawnSync } = require('child_process')
 
-const modprobe = spawnSync('sudo', ['modprobe', 'nbd'])
-if (modprobe.error) {
-  console.error(`Error: ${modprobe.error}`)
-  process.exit(1)
+const modprobe = spawnSync('sudo', ['modprobe', 'nbd'], { stdio: 'inherit' })
+if (modprobe.stderr) {
+  console.error(modprobe.stderr.toString())
+  process.exitCode = 1
 }
 
-const disconnect = spawnSync('sudo', ['nbd-client', '-d', process.argv[3]])
-if (disconnect.error) {
-  console.error(`Error: ${disconnect.error}`)
-  process.exit(1)
+const disconnect = spawnSync('sudo', ['nbd-client', '-d', process.argv[3]], { stdio: 'inherit' })
+if (disconnect.stderr) {
+  console.error(disconnect.stderr.toString())
+  process.exitCode = 1
 }
 
 const connect = spawnSync('sudo', ['nbd-client', '-N', 'export1', '-unix', process.argv[2], process.argv[3], '-b', '1024'
-])
-if (connect.error) {
-  console.error(`Error: ${connect.error}`)
-  process.exit(1)
+], { stdio: 'inherit' })
+if (connect.stderr) {
+  console.error(connect.stderr.toString())
+  process.exitCode = 1
 }
-console.log(connect.stdout.toString())
