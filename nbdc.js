fc8069719874f828e5cb938cf7d2eba89cf283c4
butera-simone <buterasimone@proton.me>
2023-04-12 02:33:11 +0200

    simplified

--- nbdc.js
+++ nbdc.js
@@ -2,21 +2,9 @@
 
 const { spawnSync } = require('child_process')
 
-const modprobe = spawnSync('sudo', ['modprobe', 'nbd'], { stdio: 'inherit' })
-if (modprobe.stderr) {
-  console.error(modprobe.stderr.toString())
-  process.exitCode = 1
-}
+spawnSync('sudo', ['modprobe', 'nbd'], { stdio: 'inherit' })
 
-const disconnect = spawnSync('sudo', ['nbd-client', '-d', process.argv[3]], { stdio: 'inherit' })
-if (disconnect.stderr) {
-  console.error(disconnect.stderr.toString())
-  process.exitCode = 1
-}
+spawnSync('sudo', ['nbd-client', '-d', process.argv[3]], { stdio: 'inherit' })
 
-const connect = spawnSync('sudo', ['nbd-client', '-N', 'export1', '-unix', process.argv[2], process.argv[3], '-b', '1024'
+spawnSync('sudo', ['nbd-client', '-N', 'export1', '-unix', process.argv[2], process.argv[3], '-b', '1024'
 ], { stdio: 'inherit' })
-if (connect.stderr) {
-  console.error(connect.stderr.toString())
-  process.exitCode = 1
-}
