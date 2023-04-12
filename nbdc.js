96f9b9eff83ed1d0f759746e3466687d0e4ee804
butera-simone <buterasimone@proton.me>
2023-04-12 03:03:01 +0200

    added check for disconnect failure

--- nbdc.js
+++ nbdc.js
@@ -4,7 +4,11 @@ const { spawnSync } = require('child_process')
 
 spawnSync('sudo', ['modprobe', 'nbd'], { stdio: 'inherit' })
 
-spawnSync('sudo', ['nbd-client', '-d', process.argv[3]], { stdio: 'inherit' })
+const disconnect = spawnSync('sudo', ['nbd-client', '-d', process.argv[3]], { stdio: 'inherit' })
+
+if (disconnect.status === 1) {
+  process.exit(1)
+}
 
 spawnSync('sudo', ['nbd-client', '-N', 'export1', '-unix', process.argv[2], process.argv[3], '-b', '1024'
 ], { stdio: 'inherit' })
